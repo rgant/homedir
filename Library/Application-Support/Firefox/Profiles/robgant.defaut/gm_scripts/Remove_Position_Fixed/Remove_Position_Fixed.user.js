015ffa8c143a7862ed5bd166f1bb78c612c49f9a
J Rob Gant <rgant@alum.wpi.edu>
2015-02-12 23:28:40 -0500

    Create Remove_Position_Fixed.user.js
    The CSS position:fixed rule is annoying

---
+++ Library/Application-Support/Firefox/Profiles/robgant.defaut/gm_scripts/Remove_Position_Fixed/Remove_Position_Fixed.user.js
@@ -0,0 +1,42 @@
+// ==UserScript==
+// @name        Remove Position Fixed
+// @namespace   name.robgant
+// @description Makes anything on the page with position fixed become static
+// @include     *
+// @version     1
+// @grant       GM_registerMenuCommand
+// @grant       GM_getValue
+// @grant       GM_setValue
+// ==/UserScript==
+
+function unfix_position() {
+//    console.log("Unfixing Positions!!");
+    Array.forEach(
+        document.querySelectorAll("div, header, section, nav")
+        ,function(el) {
+            if (window.getComputedStyle(el).position === 'fixed') {
+                el.style.position = 'static';
+                el.style.cssText += ';position:static !important;';
+            }
+        }
+    );
+}
+
+GM_registerMenuCommand("Position Unfixed", unfix_position);
+
+function set_default_unfix() {
+    var domain = window.location.host;
+    GM_setValue(domain, true);
+}
+
+GM_registerMenuCommand("Default Unfixed", set_default_unfix);
+
+var domain = window.location.host;
+if (GM_getValue(domain, false)) {
+//    console.log("Default Unfixed Position");
+    window.addEventListener("scroll", function(evt){
+        unfix_position();
+        window.removeEventListener("scroll", arguments.callee);
+    }, false);
+}
+//else { console.log("Not Default Unfixed Position"); }
