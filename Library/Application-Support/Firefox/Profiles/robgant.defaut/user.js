d5387e3d7e7188a8758ff240e7d04ef295447f0e
J Rob Gant <rgant@alum.wpi.edu>
2015-02-12 23:20:15 -0500

    Create user.js
    Firefox customizations

---
+++ Library/Application-Support/Firefox/Profiles/robgant.defaut/user.js
@@ -0,0 +1,15 @@
+user_pref("browser.backspace_action", 2);
+user_pref("browser.bookmarks.max_backups", 2);
+user_pref("browser.tabs.insertRelatedAfterCurrent", false);
+user_pref("browser.urlbar.clickSelectsAll", true);
+user_pref("browser.urlbar.trimURLs", false);
+user_pref("devtools.inspector.enabled", false);
+user_pref("devtools.styleinspector.enabled", false);
+user_pref("dom.popup_maximum", 4);
+user_pref("layout.spellcheckDefault", 2);
+user_pref("layout.word_select.eat_space_to_next_word", true);
+user_pref("network.dnsCacheEntries", 128);
+user_pref("network.dnsCacheExpriation", 3600);
+user_pref("network.http.pipelining", true);
+user_pref("network.prefetch-next", false);
+user_pref("plugins.click_to_play", true);
