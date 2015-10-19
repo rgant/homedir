17c1111a3741add0deef96d223570ac22c80c254
J Rob Gant <rgant@alum.wpi.edu>
2015-10-19 15:17:09 -0400

    Don't autoplay media. Don't hop around when closing tabs.

--- Library/Application-Support/Firefox/Profiles/robgant.default/user.js
+++ Library/Application-Support/Firefox/Profiles/robgant.default/user.js
@@ -1,6 +1,7 @@
 user_pref("browser.backspace_action", 2);
 user_pref("browser.bookmarks.max_backups", 2);
 user_pref("browser.tabs.insertRelatedAfterCurrent", false);
+user_pref("browser.tabs.selectOwnerOnClose", false);
 user_pref("browser.urlbar.clickSelectsAll", true);
 user_pref("browser.urlbar.trimURLs", false);
 user_pref("devtools.inspector.enabled", false);
@@ -8,6 +9,7 @@ user_pref("devtools.styleinspector.enabled", false);
 user_pref("dom.popup_maximum", 4);
 user_pref("layout.spellcheckDefault", 2);
 user_pref("layout.word_select.eat_space_to_next_word", true);
+user_pref("media.autoplay.enabled", false);
 user_pref("network.dnsCacheEntries", 128);
 user_pref("network.dnsCacheExpriation", 3600);
 user_pref("network.http.pipelining", true);
