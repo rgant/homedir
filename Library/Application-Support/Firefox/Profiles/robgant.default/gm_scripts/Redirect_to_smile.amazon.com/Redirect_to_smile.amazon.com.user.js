b89c103157389cc11e312baa7eeba38cdda40691
J Rob Gant <rgant@alum.wpi.edu>
2017-09-26 13:40:10 -0400

    Add redirect to smile.amazon.com for www.amazon.com pages.

---
+++ Library/Application-Support/Firefox/Profiles/robgant.default/gm_scripts/Redirect_to_smile.amazon.com/Redirect_to_smile.amazon.com.user.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name        Redirect to smile.amazon.com
+// @namespace   name.robgant
+// @description Redirect Amazon to Smile
+// @include     https://www.amazon.com/*
+// @version     1
+// @run-at      document-start
+// @grant       none
+// ==/UserScript==
+
+window.location.host = 'smile.amazon.com';
