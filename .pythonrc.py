c8ed2246fc4e93ed354de944f9f2522ed3e7745c
J Rob Gant <rgant@alum.wpi.edu>
2018-06-30 22:14:19 -0400

    Use functions for python console setup.

--- .pythonrc.py
+++ .pythonrc.py
@@ -10,6 +10,9 @@ except ImportError:
 
 import logging
 
+from pprint import pprint as pp  # pylint: disable=unused-import
+
+
 class RainbowLogFormatter(logging.Formatter):
     """ Adds a new key to the format string: %(colorlevelname)s. """
     # http://kishorelive.com/2011/12/05/printing-colors-in-the-terminal/
@@ -31,6 +34,7 @@ class RainbowLogFormatter(logging.Formatter):
         return logging.Formatter.format(self, record)
 
 def init_logging():
+    """ Sets Colorful logging for root logger. """
     frmt = '%(colorlevelname)s:%(module)s:%(funcName)s:%(lineno)d:%(message)s'
     formatter = RainbowLogFormatter(frmt)
     handler = logging.StreamHandler()
@@ -38,51 +42,54 @@ def init_logging():
     root_logger = logging.getLogger()
     root_logger.addHandler(handler)
     root_logger.setLevel(logging.DEBUG)
-    root_logger.setLevel(logging.DEBUG)
+
+def python_history():
+    """ Setup python cli history and auto complete """
+    try:
+        __IPYTHON__
+    except NameError:
+        histfilename = '.pythonhistory'
+    else:
+        histfilename = '.ipythonhistory'
+
+    try:
+        import atexit
+        import os
+        import readline
+        import rlcompleter
+    except ImportError:
+        print("Python shell enhancement modules not available.")
+    else:
+        histfile = os.path.join(os.environ["HOME"], histfilename)
+        if 'libedit' in readline.__doc__:
+            readline.parse_and_bind("bind ^I rl_complete")
+        else:
+            readline.parse_and_bind("tab: complete")
+        if os.path.isfile(histfile):
+            try:
+                readline.read_history_file(histfile)
+            except IOError:
+                pass
+        atexit.register(readline.write_history_file, histfile)
+        readline.set_history_length(1000)
+        del atexit, os, readline, rlcompleter
+        print("Python shell history and tab completion are enabled.")
+
+def set_prompt():
+    """ Colorful prompt """
+    import sys  # pylint: disable=redefined-outer-name
+    sys.ps1 = '\001\033[1;32m\002>>> \001\033[0m\002'
+    sys.ps2 = '\001\033[1;32m\002... \001\033[0m\002'
+    del sys
 
 init_logging()
+python_history()
+set_prompt()
 
-#Autocomplete and History
-try:
-    __IPYTHON__
-except NameError:
-    histfilename = '.pythonhistory'
-else:
-    histfilename = '.ipythonhistory'
-
-try:
-    import readline
-    import rlcompleter
-    import atexit
-    import os
-except ImportError:
-    print("Python shell enhancement modules not available.")
-else:
-    histfile = os.path.join(os.environ["HOME"], histfilename)
-    import rlcompleter
-    if 'libedit' in readline.__doc__:
-        readline.parse_and_bind("bind ^I rl_complete")
-    else:
-        readline.parse_and_bind("tab: complete")
-    if os.path.isfile(histfile):
-        try:
-            readline.read_history_file(histfile)
-        except IOError:
-            pass
-    atexit.register(readline.write_history_file, histfile)
-    readline.set_history_length(1000)
-    del os, histfile, readline, rlcompleter, atexit
-    print("Python shell history and tab completion are enabled.")
-
-from pprint import pprint as pp
-
-import sys
-sys.ps1 = '\001\033[1;32m\002>>> \001\033[0m\002'
-sys.ps2 = '\001\033[1;32m\002... \001\033[0m\002'
-del sys
+del init_logging, python_history, set_prompt
 
 # If there is a local "console" file when python is invoked then run it in this scope.
 try:
-    exec(open('console.py').read())
+    exec(open('console.py').read())  # pylint: disable=exec-used
 except IOError:
     pass
