25f56c330b129ad61bc15c042ca408cf6f532857
J Rob Gant <rgant@alum.wpi.edu>
2019-06-24 15:03:58 -0400

    Move list of bad subreddits outside of function

--- Greasemonkey_backup/Fix Reddit/Fix_Reddit.user.js
+++ Greasemonkey_backup/Fix Reddit/Fix_Reddit.user.js
@@ -7,51 +7,70 @@
 // ==/UserScript==
 'use strict';
 
+const head = document.getElementsByTagName('head')[0];
+if (head) {
+  const style = document.createElement('style');
+  style.textContent = '.listingsignupbar, .commentsignupbar, .organic-listing, .hover-bubble,'
+    + ' .read-next-container { display: none !important; }';
+  head.appendChild(style);
+}
+
 Array.forEach(
   document.querySelectorAll('a.choice[href="https://np.reddit.com/gilded/"]'),
   el => {
     el.href = 'https://np.reddit.com/r/popular/gilded/';
-  });
+  },
+);
 
 Array.forEach(
   document.querySelectorAll('a.title,a.comments,a.bylink'),
   el => {
     el.setAttribute('target', '_blank');
-    el.href = el.href.replace('//www.reddit.com', '//np.reddit.com');
-  }
+    el.setAttribute('href', el.href.replace('//www.reddit.com', '//np.reddit.com'));
+    el.removeAttribute('data-href-url');
+    el.removeAttribute('data-outbound-url');
+  },
 );
 
 Array.forEach(
+  document.querySelectorAll('.pinnable-content'),
+  el => {
+    el.dataset.pinCondition = () => false;
+  },
+);
+
+const badSubreddts = [
+  'AskOuija',
+  'baseball',
+  'BostonBruins',
+  'bostonceltics',
+  'CollegeBasketball',
+  'fantasyfootball',
+  'natureismetal',
+  'nba',
+  'nfl',
+  'nosleep',
+  'NYGiants',
+  'Patriots',
+  'polandball',
+  'redsox',
+  'soccer',
+  'sports',
+  'The_Donald',
+  'vegan',
+  'Warts',
+  'WritingPrompts',
+];
+Array.forEach(
   document.querySelectorAll('.subreddit'),
   el => {
-    const subreddts = [
-      'AskOuija',
-      'baseball',
-      'BostonBruins',
-      'bostonceltics',
-      'CollegeBasketball',
-      'fantasyfootball',
-      'natureismetal',
-      'nba',
-      'nfl',
-      'nosleep',
-      'NYGiants',
-      'Patriots',
-      'polandball',
-      'redsox',
-      'soccer',
-      'sports',
-      'The_Donald',
-      'vegan',
-      'Warts',
-      'WritingPrompts',
-    ];
-    for (const subr of subreddts) {
+    for (const subr of badSubreddts) {
       if (el.textContent.endsWith(subr)) {
         let thing = el.parentNode;
         while (thing && !thing.classList.contains('thing')) {
           thing = thing.parentNode;
         }
+
         thing.parentNode.removeChild(thing);
         break;
       }
@@ -65,7 +84,6 @@ if (searchEl) {
 }
 
 const entries = document.querySelectorAll('#siteTable .thing');
-
 let indx = -1;
 /**
  * Scroll posts using j and k keys. Open articles using ; key.
@@ -102,4 +120,5 @@ function pageNav(evt) {
     }
   }
 }
+
 document.addEventListener('keypress', pageNav, false);
