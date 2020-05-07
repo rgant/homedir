5be551159452c82970b7900cdbe4f0e11e1da03a
J Rob Gant <rgant@alum.wpi.edu>
2020-05-07 13:27:26 -0400

    Add fixed navigation to Miniflux

--- Greasemonkey_backup/Customize Miniflux/Customize Miniflux.user.js
+++ Greasemonkey_backup/Customize Miniflux/Customize Miniflux.user.js
@@ -2,7 +2,7 @@
 // @name        Customize Miniflux
 // @description Adds keyboard shortcut to open article in a new background tab. Fixes CSS
 // @namespace   name.robgant
-// @include     https://reader.miniflux.app/*
+// @include     https://reader.miniflux.app/*/entry/*
 // @version     1
 // @grant       GM.openInTab
 // ==/UserScript==
@@ -14,8 +14,73 @@ if (head) {
   style.textContent = [
     'body { max-width: 900px; }',
     '.entry-content figcaption { text-transform: none !important; }',
+    '.nav-container { display: grid; position: fixed; top: 0; right: 0; bottom: 0; left: 0;',
+    ' grid-template-columns: 1fr 900px 1fr; z-index: -1; }',
+    '.nav-lnk { text-align: center; text-decoration: none; font-size: 25.5vw; }',
   ].join(' ');
   head.appendChild(style);
+}
+
+const prevLnk = document.querySelector('.pagination-prev a');
+const nextLnk = document.querySelector('.pagination-next a');
+
+const goToPrev = () => {
+  history.back();
+
+  // Special case if there is a prev action.
+  if (prevLnk) {
+    setTimeout(() => {
+      window.location = prevLnk.href;
+    }, 250);
+  }
+
+  return false;
+};
+
+const goToNext = () => {
+  history.forward();
+
+  // Special case if there is a next action.
+  if (nextLnk) {
+    setTimeout(() => {
+      window.location = nextLnk.href;
+    }, 250);
+  }
+
+  return false;
+};
+
+// Add fixed navigation
+const container = document.createElement('div');
+container.className = 'nav-container';
+const pLnk = document.createElement('a');
+pLnk.className = 'nav-lnk';
+pLnk.style.gridColumn = 1;
+pLnk.textContent = '«';
+pLnk.click = goToPrev;
+if (prevLnk) {
+  prevLnk.click = goToPrev;
+  pLnk.href = prevLnk.href;
+}
+
+const nLnk = document.createElement('a');
+nLnk.className = 'nav-lnk';
+nLnk.style.gridColumn = 3;
+nLnk.textContent = '»';
+container.appendChild(pLnk);
+nLnk.click = goToNext;
+if (nextLnk) {
+  nextLnk.click = goToNext;
+  nLnk.href = nextLnk.href;
+}
+
+container.appendChild(nLnk);
+document.body.appendChild(container);
+
+// Fix encoded characters in article titles
+const lnk = document.querySelector('h1 a');
+if (lnk && lnk.innerHTML.match(/&amp;(?:\w+|#x?\d+);|&[lg]t;/)) {
+  lnk.innerHTML = lnk.textContent;
 }
 
 document.addEventListener('keydown', evt => {
@@ -35,34 +100,23 @@ document.addEventListener('keydown', evt => {
       }
     }
 
+    if (evt.key === 'l') {
+      // Ignore
+      evt.stopPropagation();
+    }
+
     // Try navigating backwards in history first, and only then go to the prev link.
     const prevKeys = ['k', 'p', 'ArrowLeft'];
     if (prevKeys.includes(evt.key)) {
       evt.stopPropagation();
-      history.back();
-
-      setTimeout(() => {
-        // Special case if there is a prev action.
-        const prevLnk = document.querySelector('.pagination-prev a');
-        if (prevLnk) {
-          prevLnk.click();
-        }
-      }, 150);
+      goToPrev();
     }
 
     // Try navigating forwards in history first, and only then go to the next link.
     const nextKeys = ['j', 'n', 'ArrowRight'];
     if (nextKeys.includes(evt.key)) {
       evt.stopPropagation();
-      history.forward();
-
-      setTimeout(() => {
-        // Special case if there is a next action.
-        const nextLnk = document.querySelector('.pagination-next a');
-        if (nextLnk) {
-          nextLnk.click();
-        }
-      }, 150);
+      goToNext();
     }
   }
 }, true);
